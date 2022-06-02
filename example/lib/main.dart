9f48bb7ba004071e72bdab25199fecd4e7039708
Nifemi <thenifemi@gmail.com>
2022-06-02 11:27:03 -0300

    example app: main.dart cleanup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_polls/flutter_polls.dart';
+
+import 'example_polls.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,40 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Polls',
       home: ExamplePolls(),
+      debugShowCheckedModeBanner: false,
     );
   }
-}
-
-class ExamplePolls extends StatefulWidget {
-  const ExamplePolls({Key? key}) : super(key: key);
-
-  @override
-  State<ExamplePolls> createState() => _ExamplePollsState();
-}
-
-class _ExamplePollsState extends State<ExamplePolls> {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('Example Polls')),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Center(
-            child: FlutterPolls(
-              pollId: 'Example',
-              onVoted: (PollOption pollOption, int newTotalVotes) {
-                // ignore: avoid_print
-                print('Voted: ${pollOption.id}');
-              },
-              pollTitle: const Text('Example Polls'),
-              pollOptions: [
-                PollOption(title: const Text('Option 1'), votes: 1),
-                PollOption(title: const Text('Option 2'), votes: 1),
-                PollOption(title: const Text('Option 3'), votes: 1),
-              ],
-            ),
-          ),
-        ),
-      );
 }
