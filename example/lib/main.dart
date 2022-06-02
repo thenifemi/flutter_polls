8ef63b61fb48902ba7d110d7508a1ba110d51155
Nifemi <thenifemi@gmail.com>
2022-06-02 16:56:46 -0300

    example app updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Polls',
+      title: 'Flutter Polls 🗳',
       home: ExamplePolls(),
       debugShowCheckedModeBanner: false,
     );
